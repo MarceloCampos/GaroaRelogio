15c62ff156df3f04094c689af1134250dfd0e6e2
Marcelo Campos <marcelo.camposs182@gmail.com>
2021-03-28 11:13:47 -0300

    Add files via upload
    Inicial

---
+++ ArduinoCode/GaroaRelogioArduinoCode1/PCICap.h
@@ -0,0 +1,22 @@
+/*
+ * Conexões da placa para capacimetro/relogio/timer
+ * Revisado Mar/21 para Placa Garoa Relógio
+ */
+ 
+const int pinBuzzer = 8;
+const int pinTec1 = 11;
+const int pinTec2 = 12;
+const int pinTec3 = 13;
+const int pinCap = 6;
+const int pinDig1 = 4;
+const int pinDig2 = 5;
+const int pinDig3 = 2;
+const int pinDig4 = 3;
+const int pinSegtoA = 7;
+const int pinSegtoB = 6;
+const int pinSegtoC = A3;
+const int pinSegtoD = A2;
+const int pinSegtoE = A1;
+const int pinSegtoF = A0;
+const int pinSegtoG = 10;
+const int pinSegtoDP = 9;
